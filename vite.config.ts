035a8984f761822655da364e58a619a3c2b12c64
kieuduc123 <ducktth2206037@fpt.edu.vn>
2025-02-19 11:16:25 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-p2p-app/",
+  base: "/my-p2p-app/", // Đúng với tên repo trên GitHub
+  build: {
+    outDir: "dist",
+  },
 })
